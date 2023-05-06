880f68849e3a19905eee5733c1ddbeeb492944fe
Cătălin <111855474+Olpalc@users.noreply.github.com>
2023-05-06 10:36:46 +0300

    done

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,29 @@
 int main()
 {
     void sayHello();
-    cout<<"Enter the first number:";
-    cin >> num1;
-    cout<<"Enter the second number:";
-    cin>>num2;
-    cout<<"Enter the operator sign:";
-    cin>>opSign;
-    calculatorThing();    
+    int numoftries;
+    cout<<"How many times would you like to calculate? :";
+    cin>> numoftries;
+    int i = 0;
+    while (i < numoftries)
+    {
+         cout<<"Enter the first number:";
+         cin >> num1;
+         cout<<"Enter the second number:";
+         cin>>num2;
+         cout<<"Enter the operator sign:";
+         cin>>opSign;
+         calculatorThing(); 
+         i++;
+         if (i = numoftries - 1)
+         {
+            cout<<"This is your last one!";
+         }
+         else
+         {
+            continue;
+         }
+            
+    }
     return 0;
 }
