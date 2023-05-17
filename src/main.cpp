3da5d6153ea5f46a0fc80e2348e2f53b53e41b20
Cătălin <111855474+Olpalc@users.noreply.github.com>
2023-05-17 20:20:54 +0300

    #done

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,25 @@
-#include "header.h"
+#include <iostream>
+
+class calculator
+{
+private:
+    double num1 , num2;
+    char opsign;
+    
+public:
+    int num_of_tries;
+    void get_all_input();
+    double calculator_thingy();
+};
+
+class calculator_func : private calculator
+{
+public:
+    double sum();
+    double sub();
+    double mul();
+    double div();
+};
 
 void say_hello()
 {
